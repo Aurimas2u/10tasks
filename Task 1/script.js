8b44334ac49cba73d45873b12cf63b066991b9dc
Aurimas2u <aurimassurkys@outlook.com>
2022-02-09 19:15:40 +0200

    1st task done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,4 +8,21 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
-//repository test2
+
+const submit = document.getElementById('submit-btn');
+const input = document.querySelector('input');
+const output = document.getElementById('output');
+
+function converter(kg){
+    output.innerHTML = '';
+    lb = kg * 2.2046;
+    g = kg / 0.0010000;
+    oz = kg * 35.274;
+    output.innerHTML += `<h1><b>${input.value}kg is equal to:</b></h1><p><em>${lb}lbs</em></p> <p><em>${g}g</em></p> <p><em>${oz}oz</em></p>`;
+    input.value = '';
+}
+
+submit.addEventListener('click', (e) =>{
+    e.preventDefault();
+    converter(input.value);
+});
