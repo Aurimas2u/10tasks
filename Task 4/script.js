1eaf0997acaadbc3422a59a7b8e7f7d41c94d297
Aurimas2u <aurimassurkys@outlook.com>
2022-02-09 21:32:25 +0200

    4th task done (uzbaigti style)

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,30 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+const output = document.getElementById('output');
+
+function showCars(){
+    fetch(ENDPOINT)
+    .then((res) =>  res.json())
+    .then((data) => {
+        drawCars(data);
+    });
+    };
+
+showCars();
+
+    function drawCars(array) {
+        for (let i = 0; i < array.length; i++) {
+        let card = document.createElement('div');
+        card.setAttribute("class", "card");
+      let brand = '';
+      let model = '';
+      brand += `<h2>${array[i].brand}</h2>`;
+      for (let a = 0; a < array[i].models.length; a++) {
+      model += `<p>${a + 1}. ${array[i].models[a]}</p>`
+    }
+    card.innerHTML += brand;
+    card.innerHTML += model;
+      output.append(card);
+        }
+    };
