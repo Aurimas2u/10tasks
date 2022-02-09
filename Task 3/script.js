8101916d1da01f78e62a6dc6bc0190bcc42c1a21
Aurimas2u <aurimassurkys@outlook.com>
2022-02-09 20:07:11 +0200

    3rd task done

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,3 +11,35 @@ Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų 
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+const message = document.getElementById('message');
+const button = document.getElementById('btn');
+const output = document.getElementById('output');
+
+function getUsers(){
+fetch(ENDPOINT)
+.then(res => res.json())
+    .then((data) => {
+    drawCards(data);
+});
+};
+
+function drawCards(array) {
+    message.remove();
+    output.style = `background-image: linear-gradient(to right, rgba(100,27,100,137), rgba(57,5,200,100)); text-align: center;`
+    for (let i = 0; i < array.length; i++) {
+    let card = document.createElement('div');
+  card.style = 'width: 120px; height: 120px; display: inline-block; margin: 10px;';
+  let photo = '';
+  let name = '';
+  photo += `<img style="width: 100px; height: 100px;" src='${array[i].avatar_url}'>`
+  name += `<p style="background-color: grey; color: white;">${array[i].login}</p>`
+  card.innerHTML += photo;
+  card.innerHTML += name;
+  output.append(card);
+    }
+};
+
+button.addEventListener('click', (e) => {
+    getUsers();
+    button.disabled = true;
+});
